54b4e96ac25e57a16cee035ee389c0c904dd8407
gpiazzon <72225531+gpiazzon@users.noreply.github.com>
2025-07-15 16:48:11 +0100

    Update build.gradle.kts

--- openTrack/build.gradle.kts
+++ openTrack/build.gradle.kts
@@ -5,12 +5,16 @@ plugins {
 }
 
 android {
-    namespace = "com.example.opentrack"
+    namespace = "com.gpiazzon.opentrack"
     compileSdk = 34
 
     defaultConfig {
         minSdk = 24
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
+    kotlinOptions { jvmTarget = "17" }
 }
 
 dependencies {
