abd33a47c81b6a476f77c1d74f17e266e2a0843a
gpiazzon <72225531+gpiazzon@users.noreply.github.com>
2025-07-16 17:09:03 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.compose.ui:ui:1.6.1")
     implementation("androidx.compose.ui:ui-tooling-preview:1.6.1")
-    implementation("androidx.compose.material3:material3:1.2.0")
+    implementation("androidx.compose.material3:material3:1.2.0-alpha13")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.compose.material:material-icons-extended:1.6.1")
     implementation("androidx.work:work-runtime-ktx:2.9.0")
